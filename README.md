a9c309d8881921d7c1f7f077e3adbfac49b158ef
JSONRete <jsonrete@gmail.com>
2022-08-28 21:35:06 -0400

    stuck

--- README.md
+++ README.md
@@ -33,6 +33,10 @@ Ruby
 
 ## Licensing
 
+<<<<<<< HEAD
 This application is licensed under the [MIT License](LICENSE).
 
 gg
+=======
+This application is licensed under the [MIT License](LICENSE).
+>>>>>>> 77d275c (readded temp readme)
--- js/app.js
+++ js/app.js
@@ -1,36 +1,7 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
 const navbarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+
 
 sections.forEach(function(section) {
   
@@ -45,35 +16,12 @@ sections.forEach(function(section) {
   navbarList.appendChild(li);
 });
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
 
 
-// Add class 'active' to section when near top of viewport
 
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 window.addEventListener('scroll', function() {
     sections.forEach(function(section) {
-      // check the position
+
       const screen = section.getBoundingClientRect();
       const test = screen.top > 0;
       
