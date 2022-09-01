164c985d96b2014a89aaf2067b06a9d13176310b
JSONRete <jsonrete@gmail.com>
2022-08-31 23:47:34 -0400

    added some comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,38 @@
+// Global Variables
 const navbarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
-sections.forEach(function(section) {
-  
+
+// Builds the nav
+sections.forEach( (section) => {
+  // builds menu items
   const li = document.createElement('li');
 
   li.innerHTML = `<a class='menu__link'>${section.dataset.nav}</a>`;
-  li.addEventListener('click', function() {
+  // Scroll to section on link click
+  li.addEventListener('click', () => {
     section.scrollIntoView({behavior: "smooth"});
   });
-  
+  // places built menu on navbar
   navbarList.appendChild(li);
 });
 
+// detects the scroll
+window.addEventListener('scroll', () => {
+    sections.forEach( (section) => {
 
-window.addEventListener('scroll', function() {
-    sections.forEach(function(section) {
-
-      const screen = section.getBoundingClientRect();
-      const test = screen.top > 33;
-      
-      if (test) {
+      const inView = section.getBoundingClientRect();
+      const position = inView.top > -5;
+      // Set sections as active
+      if (position) {
         section.classList.add('your-active-class');
+        document.getElementById(`${section.dataset.nav}`).navbarList.add('active');
+        
+
       } else {
         section.classList.remove('your-active-class');
       }
     });
   })
+
+  
