ad6f98758432e6be392bf343f803418d3ce16592
JSONRete <jsonrete@gmail.com>
2022-09-04 02:19:15 -0400

    finishing JavaScript

--- js/app.js
+++ js/app.js
@@ -1,30 +1,28 @@
-// Global Variables
+  // Global Variables
 const navbarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
-// Builds the nav
+  // Builds the nav
 sections.forEach( (section) => {
-  // builds menu items
+  // Builds menu items
   const li = document.createElement('li');
-  // creating li element
+  // Creating li element
   li.setAttribute('data-id', section.getAttribute('id'));
   li.innerHTML = `<a class='menu__link'>${section.dataset.nav}</a>`;
   // Scroll to section on link click
   li.addEventListener('click', () => {
     section.scrollIntoView({behavior: "smooth"});
   });
-  // places built menu on navbar
+  // Places built menu on navbar
   navbarList.appendChild(li);
 });
-// detects the scroll
+  // Detects scroll
 window.addEventListener('scroll', () => {
     sections.forEach( (section) => {
 
       const inView = section.getBoundingClientRect();
       const id = section.getAttribute('id');
-      const position =  inView.top <= 150 && inView.bottom >= 150
-
-      // inView.top <= 150 && inView.bottom >= 150
-      // Set sections as active
+      const position =  inView.y <= 175 && inView.bottom >= 175
+      // section active control
       if (position) {
         section.classList.add('your-active-class');
         navbarList.querySelector(`[data-id=${id}]`).classList.add('navbar-section-active');
@@ -35,5 +33,3 @@ window.addEventListener('scroll', () => {
       }
     });
   })
-
-  
