e2c29f3940cbd3e1840907d780366ffe16f48060
JSONRete <jsonrete@gmail.com>
2022-09-11 23:59:57 -0400

    git commit -m “cleaned up comments”

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
-  // Global Variables
+  // global variables
 const navbarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
-  // Builds the nav
+  // builds the nav
 sections.forEach( (section) => {
-  // Builds menu items
+  // builds menu items
   const li = document.createElement('li');
-  // Creating li element
+  // creating li element
   li.setAttribute('data-id', section.getAttribute('id'));
   li.innerHTML = `<a class='menu__link'>${section.dataset.nav}</a>`;
-  // Scroll to section on link click
+  // scroll to section on link click
   li.addEventListener('click', () => {
     section.scrollIntoView({behavior: "smooth"});
   });
-  // Places built menu on navbar
+  // places built menu on navbar
   navbarList.appendChild(li);
 });
-  // Detects scroll
+  // detects scroll
 window.addEventListener('scroll', () => {
     sections.forEach( (section) => {
-      // getBoundingClientRect() method returns a DOMRect object providing information about the size of an element and its position relative to the viewport.
+      // getBoundingClientRect() method returns a DOMRect object providing information about the size of an element and its position relative to the viewport
       const inView = section.getBoundingClientRect();
       const id = section.getAttribute('id');
       const position =  inView.y <= 175 && inView.bottom >= 175
